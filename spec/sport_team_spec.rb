4d06fc6bd950132c4968c7586f53fb45a0177c6d
oksana richards <oksana_vldk@hotmail.com>
2018-09-17 16:08:51 +0100

    add players

--- spec/sport_team_spec.rb
+++ spec/sport_team_spec.rb
@@ -24,4 +24,19 @@ class TestSportTeam < MiniTest::Test
     assert_equal("Bill",result)
   end
 
+  def test_change_coach_name
+    @sport_team.coach = "Daniel"
+    result = @sport_team.get_coach
+    assert_equal("Daniel",result)
+  end
+
+  def test_add_players
+    @sport_team.add_players(["Craig","Adam"])
+    result = @sport_team.players.length
+    assert_equal(7,result)
+  end 
+
+
+
+
 end
