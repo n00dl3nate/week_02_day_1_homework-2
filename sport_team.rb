57aa916f662abf4211c60184d5742fb1cc86341b
oksana richards <oksana_vldk@hotmail.com>
2018-09-18 09:29:48 +0100

    all exercises

--- sport_team.rb
+++ sport_team.rb
@@ -1,10 +1,11 @@
 class SportTeam
+ attr_accessor :team_name, :players, :coach, :points
 
-
-def initialize(team_name, players, coach)
+def initialize(team_name, players, coach, points)
   @team_name = team_name
   @players = players
   @coach = coach
+  @points = points
 end
 
 def get_team_name
@@ -18,6 +19,29 @@ end
 def get_coach
   return @coach
 end
+
+def add_players(players)
+  @players += players
+end
+
+def check_players(player)
+  for person in @players
+    if person == player
+      return true
+    else
+      return false
+    end
+  end
+end
+
+def team_result(result)
+  if result == "win"
+    @points += 3
+  elsif result == "draw"
+    @points += 1
+  end
+end
+
 
 
 
